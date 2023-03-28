e45be933110c082db81586e8cc5ca8955929e411
chenxing <chenxing640@foxmail.com>
2023-03-28 16:25:20 +0800

    [::] Fetch an image for taking screenshot by detector.

---
+++ CXSwiftKit/Classes/Core/CXTakeScreenshotDetector.swift
@@ -0,0 +1,52 @@
+//
+//  CXTakeScreenshotDetector.swift
+//  CXSwiftKit
+//
+//  Created by chenxing on 2023/3/16.
+//
+
+#if canImport(Photos) && canImport(UIKit)
+import UIKit
+import Photos
+
+public class CXTakeScreenshotDetector: NSObject {
+    
+    public override init() {
+        super.init()
+        self.setup()
+    }
+    
+    private var takeScreenshotHandler: ((UIImage?) -> Void)?
+    
+    private func setup() {
+        self.cx.addObserver(self, selector: #selector(userDidTakeScreenshot(_:)), name: UIApplication.userDidTakeScreenshotNotification)
+    }
+    
+    @objc private func userDidTakeScreenshot(_ notification: Notification) {
+        CXLogger.log(level: .info, message: "The user takes screenshot.")
+        if CXPermissionManager.shared.photoLibraryAuthorized {
+            DispatchQueue.cx.mainAsyncAfter(3.0) {
+                self.fetchImage()
+            }
+        } else {
+            CXLogger.log(level: .error, message: "The user didn't granted this app access to the photo library.")
+        }
+    }
+    
+    private func fetchImage() {
+        CXPermissionManager.shared.fetchLatestImage { [unowned self] imageData in
+            self.takeScreenshotHandler?(imageData != nil ? UIImage(data: imageData!) : nil)
+        }
+    }
+    
+    public func detect(handler: @escaping (UIImage?) -> Void) {
+        takeScreenshotHandler = handler
+    }
+    
+    deinit {
+        self.cx.removeObserver(self, name: UIApplication.userDidTakeScreenshotNotification)
+    }
+    
+}
+
+#endif
