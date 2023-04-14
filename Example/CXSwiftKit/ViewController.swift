5a3eee1553ccf18cd64d8f290b007feffecfa62b
chenxing <chenxing640@foxmail.com>
2023-04-14 17:28:34 +0800

    [::] U

--- Example/CXSwiftKit/ViewController.swift
+++ Example/CXSwiftKit/ViewController.swift
@@ -13,12 +13,6 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let url = URL(string: "https://www.baidu.com/432432/afdfsf.jpg") {
-            let fileName = CXFileToolbox.fileName(withURL: url)
-            let filePath = CXFileToolbox.filePath(withURL: url, atDirectory: "cx.swkit.dir")
-            CXLogger.log(level: .info, message: "fileName=\(fileName), filePath=\(filePath)")
-            CXFileToolbox.write(data: Data(contentsOf: url), toPath: filePath)
-        }
     }
     
     override func didReceiveMemoryWarning() {
