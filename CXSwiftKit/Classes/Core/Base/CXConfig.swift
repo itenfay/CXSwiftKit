b5595fc463f212d5769175bae29d237831a99e8e
chenxing <chenxing640@foxmail.com>
2023-04-19 15:33:01 +0800

    [::] Update app configuration.

--- CXSwiftKit/Classes/Core/Base/CXConfig.swift
+++ CXSwiftKit/Classes/Core/Base/CXConfig.swift
@@ -8,18 +8,31 @@
 #if canImport(Foundation)
 import Foundation
 
-@objc public enum AppConfiguration: UInt8 {
+@objc public enum AppConfiguration: UInt8, CustomStringConvertible {
+    
     case debug
+    case adHoc
+    case inHouse
     case testFlight
-    case enterprise
     case appStore
+    
+    public var description: String {
+        switch self {
+        case .debug: return "Debug"
+        case .adHoc: return "AdHoc"
+        case .inHouse: return "InHouse" //Enterprise
+        case .testFlight: return "TestFlight"
+        case .appStore: return "AppStore"
+        }
+    }
+    
 }
 
 public class CXConfig: NSObject {
     
     /// This is private because the use of 'appConfiguration' is preferred.
     private static var isTestFlight: Bool { Bundle.main.appStoreReceiptURL?.lastPathComponent == "sandboxReceipt" }
-    private static var hasEmbeddedProvision: Bool { Bundle.main.path(forResource: "embedded", ofType: "mobileprovision") != nil }
+    @objc public static var embeddedProvisionPath: String { Bundle.main.path(forResource: "embedded", ofType: "mobileprovision") ?? "" }
     
     /// This can be used to add debug statements.
     @objc public static var isDebug: Bool {
@@ -35,8 +48,26 @@ public class CXConfig: NSObject {
             return .debug
         } else if isTestFlight {
             return .testFlight
-        } else if hasEmbeddedProvision {
-            return .enterprise
+        } else if !embeddedProvisionPath.isEmpty {
+            if let reader = CXLineReader(path: embeddedProvisionPath) {
+                while let line = reader.nextLine {
+                    if line.contains("<key>Name</key>") {
+                        let nextLine = reader.nextLine?.lowercased() ?? ""
+                        if nextLine.contains("ad hoc") ||
+                            nextLine.contains("adhoc") {
+                            return .adHoc
+                        } else if nextLine.contains("inhouse") {
+                            return .inHouse
+                        }
+                    }
+                    if line.contains("<key>ProvisionsAllDevices</key>") {
+                        return .inHouse
+                    } else if line.contains("<key>ProvisionedDevices</key>") {
+                        return .adHoc
+                    }
+                }
+            }
+            return .adHoc
         } else {
             return .appStore
         }
@@ -46,7 +77,7 @@ public class CXConfig: NSObject {
 
 /// The config for the log.
 @objcMembers public class CXLogConfig: NSObject {
-    /// Whether to enable log, the default is false.
+    /// Whether to enable log, default is false.
     public static var enableLog: Bool = false
 }
 
