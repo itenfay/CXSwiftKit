8291a983ec78b1ee8a0b3cb7897a690cd3ccc134
chenxing <chenxing640@foxmail.com>
2023-04-25 10:03:59 +0800

    [::] Add the properties of empty data style.

--- CXSwiftKit/Classes/Core/EmptyDataSet/CXEmptyDataSetStyle.swift
+++ CXSwiftKit/Classes/Core/EmptyDataSet/CXEmptyDataSetStyle.swift
@@ -11,21 +11,46 @@ import Foundation
 //MARK: - EmptyDataSetStyle
 
 public class CXEmptyDataSetStyle: NSObject {
+    
+    /// The background color for empty data set.
+    @objc public var backgroundColor: UIColor = UIColor.cx.color(withHexString: "#FFFFFF")!
+    /// The title for empty data set.
     @objc public var title: String?
-    @objc public var titleColor: UIColor?
+    /// The title color for empty data set.
+    @objc public var titleColor: UIColor = UIColor.cx.color(withHexString: "#333333")!
+    /// The title font for empty data set.
+    @objc public var titleFont: UIFont = UIFont.cx.regularPingFang(ofSize: 13)
+    
+    /// The description for empty data set.
+    @objc public var descriptionString: String?
+    /// The description color for empty data set.
+    @objc public var descriptionColor: UIColor = UIColor.cx.color(withHexString: "#333333")!
+    /// The description font for empty data set.
+    @objc public var descriptionFont: UIFont = UIFont.cx.regularPingFang(ofSize: 13)
+    
+    /// The image font for empty data set.
     @objc public var image: UIImage?
+    /// The loading animated image for empty data set.
+    @objc public var loadingAnimatedImage: UIImage?
     
-    @objc public static var emptyStyle: CXEmptyDataSetStyle {
-        let style = CXEmptyDataSetStyle()
-        style.title = "暂无数据"
-        return style
-    }
+    /// The button title for empty data set.
+    @objc public var buttonTitle: String?
+    /// The button color for empty data set.
+    @objc public var buttonColor: UIColor = UIColor.cx.color(withHexString: "#9D1420")!
+    /// The button font for empty data set.
+    @objc public var buttonFont: UIFont = UIFont.cx.regularPingFang(ofSize: 13)
+    /// The button normal image font for empty data set.
+    @objc public var buttonNormalImage: UIImage?
+    /// The button highlighted image font for empty data set.
+    @objc public var buttonHighlightedImage: UIImage?
     
-    @objc public static var networkErrorStyle: CXEmptyDataSetStyle {
-        let style = CXEmptyDataSetStyle()
-        style.title = "服务异常，网络断开或飞行模式"
-        return style
-    }
+    /// The vertical offset for empty data set.
+    @objc public var verticalOffset: CGFloat = 0
+    /// The vertical distance for empty data set.
+    @objc public var verticalSpace: CGFloat = 20
+    
+    @objc public static var emptyTitle: String = "暂无数据"
+    @objc public static var networkErrorTitle: String = "服务异常，网络断开或飞行模式"
     
 }
 
