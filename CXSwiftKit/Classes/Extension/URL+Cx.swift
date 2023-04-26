9277f3c2ab53c6c9d9b4f2b8584f9aa5d9915ffa
chenxing <chenxing640@foxmail.com>
2023-04-26 11:49:46 +0800

    [::] Add the cx_path to resolve deprecated api

--- CXSwiftKit/Classes/Extension/URL+Cx.swift
+++ CXSwiftKit/Classes/Extension/URL+Cx.swift
@@ -53,6 +53,9 @@ extension CXSwiftBase where T == URL {
         return base.cx_dropScheme()
     }
     
+    /// Return the path, or an empty string if the URL has an empty path.
+    public var path: String { base.cx_path }
+    
 }
 
 #if canImport(AVFoundation)
@@ -163,6 +166,15 @@ extension URL {
         guard host != nil else { return absoluteString }
         
         return String(absoluteString.dropFirst(2))
+    }
+    
+    /// Return the path, or an empty string if the URL has an empty path.
+    public var cx_path: String {
+        if #available(macOS 13.0, iOS 16.0, tvOS 16.0, watchOS 9.0, *) {
+            return path(percentEncoded: false)
+        } else {
+            return path
+        }
     }
     
 }
