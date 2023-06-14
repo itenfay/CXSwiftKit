c1159c0d6ead7b089af76ecb3b65b0a8c4f3da5b
chenxing <chenxing640@foxmail.com>
2023-06-14 17:26:33 +0800

    update

--- CXSwiftKit/Classes/Core/AR/CXMetalVideoRecorder.swift
+++ CXSwiftKit/Classes/Core/AR/CXMetalVideoRecorder.swift
@@ -19,7 +19,7 @@ public class CXMetalVideoRecorder: NSObject {
     
     private var assetWriter: AVAssetWriter
     private var assetWriterVideoInput: AVAssetWriterInput
-    private var assetWriterPixelBufferInput: AVAssetWriterInputPixelBufferAdaptor
+    private var assetWriterPixelBufferAdaptor: AVAssetWriterInputPixelBufferAdaptor
     
     @objc public let outputURL: URL
     @objc public var isRecording = false
@@ -39,38 +39,28 @@ public class CXMetalVideoRecorder: NSObject {
         }
         
         // AVVideoCodecType.h264 larger file size
-        let outputSettings: [String: Any] = [AVVideoCodecKey : AVVideoCodecType.hevc,
-                                             AVVideoWidthKey : size.width/2,
-                                            AVVideoHeightKey : size.height/2,
-                             AVVideoCompressionPropertiesKey : [
-                                AVVideoQualityKey : 1.0,
-                                //AVVideoMaxKeyFrameIntervalKey : 5,
-                                //AVVideoAverageBitRateKey : 25500000
-                             ]
-        ]
-        
+        let outputSettings: [String: Any] = [AVVideoCodecKey : AVVideoCodecType.h264,
+                                             AVVideoWidthKey : size.width,
+                                            AVVideoHeightKey : size.height]
         assetWriterVideoInput = AVAssetWriterInput(mediaType: AVMediaType.video, outputSettings: outputSettings)
         assetWriterVideoInput.expectsMediaDataInRealTime = true
         
         let sourcePixelBufferAttributes: [String: Any] = [
-            kCVPixelBufferPixelFormatTypeKey as String : kCVPixelFormatType_32BGRA,
-            kCVPixelBufferWidthKey as String : size.width,
-            kCVPixelBufferHeightKey as String : size.height]
+            kCVPixelBufferPixelFormatTypeKey as String : kCVPixelFormatType_32BGRA
+        ]
+        assetWriterPixelBufferAdaptor = AVAssetWriterInputPixelBufferAdaptor(assetWriterInput: assetWriterVideoInput,
+                                                                             sourcePixelBufferAttributes: sourcePixelBufferAttributes)
+        //CXLogger.log(level: .info, message: "PixelBuffer input: \(assetWriterPixelBufferAdaptor)")
         
-        assetWriterPixelBufferInput = AVAssetWriterInputPixelBufferAdaptor(assetWriterInput: assetWriterVideoInput,
-                                                                           sourcePixelBufferAttributes: sourcePixelBufferAttributes)
-        //CXLogger.log(level: .info, message: "PixelBuffer input: \(assetWriterPixelBufferInput)")
         assetWriter.add(assetWriterVideoInput)
     }
     
     /// Deletes the output file.
     @objc public func deleteOutputFile() {
-        if FileManager.default.fileExists(atPath: outputURL.cx.path) {
-            do {
-                try FileManager.default.removeItem(at: outputURL)
-            } catch let error {
-                CXLogger.log(level: .info, message: "Delete the output file: \(error.localizedDescription)")
-            }
+        do {
+            try FileManager.default.removeItem(at: outputURL)
+        } catch let error {
+            CXLogger.log(level: .info, message: "\(error.localizedDescription)")
         }
     }
     
@@ -112,10 +102,11 @@ public class CXMetalVideoRecorder: NSObject {
         if !isRecording {
             return
         }
-        while !assetWriterVideoInput.isReadyForMoreMediaData {
+        if !assetWriterVideoInput.isReadyForMoreMediaData {
             //CXLogger.log(level: .error, message: "Not ready for more media data")
+            return
         }
-        guard let pixelBufferPool = assetWriterPixelBufferInput.pixelBufferPool else {
+        guard let pixelBufferPool = assetWriterPixelBufferAdaptor.pixelBufferPool else {
             //CXLogger.log(level: .error, message: "Pixel buffer asset writer input did not have a pixel buffer pool available; cannot retrieve frame")
             return
         }
@@ -127,7 +118,7 @@ public class CXMetalVideoRecorder: NSObject {
             return
         }
         guard let pixelBuffer = maybePixelBuffer else {
-            CXLogger.log(level: .error, message: "NO pixelBuffer")
+            CXLogger.log(level: .error, message: "No pixelBuffer")
             return
         }
         CVPixelBufferLockBaseAddress(pixelBuffer, [])
@@ -136,15 +127,15 @@ public class CXMetalVideoRecorder: NSObject {
         // Use the bytes per row value from the pixel buffer since its stride may be rounded up to be 16-byte aligned
         let bytesPerRow = CVPixelBufferGetBytesPerRow(pixelBuffer)
         let region = MTLRegionMake2D(0, 0, texture.width, texture.height)
-        
+        // Insert all the texture processing logic
         texture.getBytes(pixelBufferBytes, bytesPerRow: bytesPerRow, from: region, mipmapLevel: 0)
         
         let frameTime = CACurrentMediaTime() - recordingStartTime
         let presentationTime = CMTimeMakeWithSeconds(frameTime, preferredTimescale: 240)
-        assetWriterPixelBufferInput.append(pixelBuffer, withPresentationTime: presentationTime)
+        assetWriterPixelBufferAdaptor.append(pixelBuffer, withPresentationTime: presentationTime)
+        outPixelBuffer = pixelBuffer
         
         CVPixelBufferUnlockBaseAddress(pixelBuffer, [])
-        outPixelBuffer = pixelBuffer
     }
     #endif
     
