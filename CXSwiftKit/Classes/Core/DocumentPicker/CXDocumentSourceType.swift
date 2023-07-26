87bdc96efa02f5560b7d51b28b7d8ddf67f08563
chenxing <chenxing640@foxmail.com>
2023-07-26 17:58:14 +0800

    add  enum

--- CXSwiftKit/Classes/Core/DocumentPicker/CXDocumentSourceType.swift
+++ CXSwiftKit/Classes/Core/DocumentPicker/CXDocumentSourceType.swift
@@ -6,11 +6,13 @@
 //
 
 @objc public enum CXDocumentSourceType: UInt8, CustomStringConvertible {
+    case none
     case file
     case folder
     
     public var description: String {
         switch self {
+        case .none: return "None"
         case .file: return "File"
         case .folder: return "Folder"
         }
