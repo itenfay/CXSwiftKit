eb57bb72753d1f2c111e67df27a83730b626e907
chenxing <chenxing640@foxmail.com>
2023-07-28 14:14:19 +0800

    this class conforms to protocol

--- CXSwiftKit/Classes/Core/AVToolbox/CXAudioRecorder.swift
+++ CXSwiftKit/Classes/Core/AVToolbox/CXAudioRecorder.swift
@@ -22,32 +22,51 @@ import AVFAudio
     }
 }
 
-public class CXAudioRecorder: NSObject {
+@objc public protocol ISKAudioRecorder: AnyObject {
+    var isRecording: Bool { get }
+    var category: CXRecordingOptionCategory { get }
+    var filePath: String { get }
+    func makeFilePath(fileName: String) -> String
+    var onFinish: (() -> Void)? { get set }
+    var onError: ((String) -> Void)? { get set }
+    func prepareToRecord()
+    func prepareToRecord(_ fileName: String, category: CXRecordingOptionCategory)
+    func prepareToRecord(_ fileName: String, category: CXRecordingOptionCategory, configuration: (() -> [String : Any])?)
+    func prepareToRecord(_ fileName: String, category: CXRecordingOptionCategory, configuration: (() -> [String : Any])?, toMP3: Bool)
+    func record() -> Bool
+    func pause()
+    func stop()
+    func deleteRecording()
+    func calcDecibels() -> Float
+    var level: Float { get }
+}
+
+public class CXAudioRecorder: NSObject, ISKAudioRecorder {
     
     /// Audio recorder that records audio data to a file.
     private var recorder: AVAudioRecorder?
     
     /// The option category for the recording.
-    private var category: CXRecordingOptionCategory = .speaker
+    public private(set) var category: CXRecordingOptionCategory = .speaker
     
     /// The path of a recorded audio file.
-    @objc public private(set) var filePath: String = ""
+    public private(set) var filePath: String = ""
     
     /// The closure for finishing recording.
-    @objc public var onFinish: (() -> Void)?
+    public var onFinish: (() -> Void)?
     /// The closure for the error.
-    @objc public var onError: ((String) -> Void)?
+    public var onError: ((String) -> Void)?
     
     /// A Boolean value that indicates whether the audio recorder is recording.
-    @objc public var isRecording: Bool {
+    public var isRecording: Bool {
         return recorder?.isRecording ?? false
     }
     
-    @objc public func exportFilePath(fileName: String? = nil) -> String {
+    public func makeFilePath(fileName: String) -> String {
         // PCM
         var prefixName = ""
-        if let fn = fileName, fn.cx.isNotEmpty() {
-            prefixName = fn
+        if fileName.cx.isNotEmpty() {
+            prefixName = fileName
         } else {
             let random = Int(arc4random() % 10000) + 1
             let formatter = DateFormatter()
@@ -73,19 +92,19 @@ public class CXAudioRecorder: NSObject {
        AVEncoderAudioQualityKey: AVAudioQuality.medium.rawValue]
     }
     
-    @objc public func prepareToRecord() {
+    public func prepareToRecord() {
         prepareToRecord("", category: .speaker)
     }
     
-    @objc public func prepareToRecord(_ fileName: String, category: CXRecordingOptionCategory) {
+    public func prepareToRecord(_ fileName: String, category: CXRecordingOptionCategory) {
         prepareToRecord(fileName, category: category, configuration: nil)
     }
     
-    @objc public func prepareToRecord(_ fileName: String, category: CXRecordingOptionCategory, configuration: (() -> [String : Any])?) {
+    public func prepareToRecord(_ fileName: String, category: CXRecordingOptionCategory, configuration: (() -> [String : Any])?) {
         prepareToRecord(fileName, category: category, configuration: configuration, toMP3: false)
     }
     
-    @objc public func prepareToRecord(_ fileName: String, category: CXRecordingOptionCategory, configuration: (() -> [String : Any])?, toMP3: Bool) {
+    public func prepareToRecord(_ fileName: String, category: CXRecordingOptionCategory, configuration: (() -> [String : Any])?, toMP3: Bool) {
         self.category = category
         
         let session = AVAudioSession.sharedInstance()
@@ -111,7 +130,7 @@ public class CXAudioRecorder: NSObject {
         }
         CXLogger.log(level: .info, message: "settings=\(settings)")
         
-        filePath = exportFilePath(fileName: fileName)
+        filePath = makeFilePath(fileName: fileName)
         CXLogger.log(level: .info, message: "filePath=\(filePath)")
         if filePath.isEmpty {
             onError?("The file path is empty.")
@@ -132,17 +151,17 @@ public class CXAudioRecorder: NSObject {
     
     /// Starts or resumes audio recording.
     @discardableResult
-    @objc public func record() -> Bool {
+    public func record() -> Bool {
         return recorder?.record() ?? false
     }
     
     /// Pauses an audio recording.
-    @objc public func pause() {
+    public func pause() {
         recorder?.pause()
     }
     
     /// Stops recording and closes the audio file.
-    @objc public func stop() {
+    public func stop() {
         recorder?.stop()
         let session = AVAudioSession.sharedInstance()
         if category == .quiet {
@@ -154,28 +173,50 @@ public class CXAudioRecorder: NSObject {
     }
     
     /// Deletes a recorded audio file.
-    @objc public func deleteRecording() {
+    public func deleteRecording() {
         recorder?.stop()
         recorder?.deleteRecording()
     }
     
     /// Calculates the decibels of a recording voice.
-    @objc public func calculateDecibels() -> Float {
+    public func calcDecibels() -> Float {
         // Update the level meter data before calling.
         recorder?.updateMeters()
         // Average value
         var avg: Float = recorder?.averagePower(forChannel: 0) ?? 0
         // Peak value
-        //let peak: Float = recorder?.peakPower(forChannel: 0) ?? 0
-        let minValue: Float = -60
-        let range: Float = 60
-        let outRange: Float = 100
+        //let peak: Float = recorder?.peakPower(forChannel: 0) ?? 0
+        let minValue: Float = -60.0
+        let range: Float = 60.0
+        let outRange: Float = 100.0
         if avg < minValue {
             avg = minValue
         }
         // decibels
         let decibels = (avg + range) / range * outRange
         return decibels
+    }
+    
+    /// Gets the level of a recording voice.
+    /// http://stackoverflow.com/questions/9247255/am-i-doing-the-right-thing-to-convert-decibel-from-120-0-to-0-120/16192481#16192481
+    public var level: Float {
+        recorder?.updateMeters()
+        var level: Float = 0.0 // The linear 0.0 .. 1.0 value we need.
+        let minDecibels: Float = -80.0 // Or use -60dB, which I measured in a silent room.
+        let decibels = recorder?.averagePower(forChannel: 0) ?? 0
+        if decibels < minDecibels {
+            level = 0.0
+        } else if decibels >= 0.0 {
+            level = 1.0
+        } else {
+            let root: Float = 2.0
+            let minAmp: Float = powf(10.0, 0.05 * minDecibels)
+            let inverseAmpRange: Float = 1.0 / (1.0 - minAmp)
+            let amp: Float = powf(10.0, 0.05 * decibels)
+            let adjAmp: Float = (amp - minAmp) * inverseAmpRange
+            level = powf(adjAmp, 1.0 / root)
+        }
+        return level
     }
     
 }
@@ -185,7 +226,9 @@ public class CXAudioRecorder: NSObject {
 extension CXAudioRecorder: AVAudioRecorderDelegate {
     
     public func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
-        if flag { onFinish?() }
+        if flag {
+            onFinish?()
+        }
     }
     
 }
