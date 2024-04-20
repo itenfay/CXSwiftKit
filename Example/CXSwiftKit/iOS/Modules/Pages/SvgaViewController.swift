cff975ae206e95a7d493c494404ae068ea04a42b
Teng Fei <hansen981@126.com>
2024-04-21 00:25:14 +0800

    add the usage of queue's priority

--- Example/CXSwiftKit/iOS/Modules/Pages/SvgaViewController.swift
+++ Example/CXSwiftKit/iOS/Modules/Pages/SvgaViewController.swift
@@ -16,8 +16,10 @@ import SVGAPlayer
 class SvgaViewController: BaseViewController {
     
     private var svgaPlayer: SVGAPlayer!
-    
     private let disposeBag = DisposeBag()
+    private var firstOps: [MarsSVGAPlayOperation] = []
+    private var firstPercentages: [String : CGFloat] = [:]
+    private var lastOp: MarsSVGAPlayOperation!
     
     private let items = [
         "https://cdn.jsdelivr.net/gh/svga/SVGA-Samples@master/EmptyState.svga?raw=true",
@@ -116,6 +118,23 @@ class SvgaViewController: BaseViewController {
             self?.play(with: 4)
         }).disposed(by: disposeBag)
         
+        let btnE = UIButton(type: .custom)
+        btnE.backgroundColor = .gray
+        btnE.setTitle("优先显示", for: .normal)
+        btnE.titleLabel?.font = UIFont.cx.mediumPingFang(ofSize: 16)
+        btnE.layer.cornerRadius = 10
+        btnE.showsTouchWhenHighlighted = true
+        view.addSubview(btnE)
+        btnE.cx.makeConstraints { maker in
+            maker.top.equalToAnchor(btnA.bottomAnchor).offset(paddingY)
+            maker.leading.equalToAnchor(btnA.leadingAnchor)
+            maker.width.equalTo(btnW)
+            maker.height.equalTo(btnH)
+        }
+        btnE.rx.tap.asDriver().drive(onNext: { [weak self] in
+            self?.play(with: 5)
+        }).disposed(by: disposeBag)
+        
         svgaPlayer = SVGAPlayer()
         svgaPlayer.contentMode = .bottom
         view.addSubview(svgaPlayer)
@@ -128,7 +147,7 @@ class SvgaViewController: BaseViewController {
     }
     
     private func bindSvgaPlayer() {
-        MarsSvgaPlayManager.shared.svgaPlayer = svgaPlayer
+        MarsSVGAPlayManager.shared.svgaPlayer = svgaPlayer
     }
     
     func play(with tag: Int) {
@@ -136,20 +155,82 @@ class SvgaViewController: BaseViewController {
         UIDevice.current.cx.makeImpactFeedback(style: .heavy)
         //UIDevice.current.cx.makeSelectionFeedback()
         //UIDevice.current.cx.makeNotificationFeedback(type: .success)
-        //CXHaptics.strongBoom()
-        //CXHaptics.weakBoom()
-        //CXHaptics.threeWeakBooms()
+        
+        let svgaPlayManager = MarsSVGAPlayManager.shared
         if tag == 1 {
-            MarsSvgaPlayManager.shared.play(named: "Rocket")
+            svgaPlayManager.play(named: "Rocket")
         } else if tag == 2 {
-            MarsSvgaPlayManager.shared.play(named: "heartbeat")
+            svgaPlayManager.play(named: "heartbeat")
         } else if tag == 3 {
-            MarsSvgaPlayManager.shared.play(named: "rose_2.0.0")
+            svgaPlayManager.play(named: "rose_2.0.0")
         } else if tag == 4 {
             let i = Int(arc4random_uniform(UInt32(items.count)))
             let url = items[i < items.count ? i : 0]
-            MarsSvgaPlayManager.shared.play(url: url)
+            svgaPlayManager.play(url: url)
+        } else if tag == 5 {
+            CXHaptics.strongBoom()
+            //CXHaptics.weakBoom()
+            //CXHaptics.threeWeakBooms()
+            // Extracts the specified operation.
+            svgaPlayManager.play(url: "https://cdn.jsdelivr.net/gh/svga/SVGA-Samples@master/posche.svga?raw=true")
+            
+            let ops = svgaPlayManager.operations
+            ops.forEach { op in
+                CXLog.info("op: \(op), \(String(describing: op.svgaUrl)) or \(String(describing: op.svgaName))")
+            }
+            
+            // 获取最后一个任务和第一个任务，更改最后一个任务的优先级，让最后一个先执行。
+            if let lastOp = ops.last, let firstOp = ops.first, lastOp !== firstOp {
+                if !firstOps.contains(firstOp) {
+                    firstOp.queuePriority = .normal
+                    firstOps.append(firstOp)
+                }
+                self.lastOp = lastOp
+                svgaPlayManager.onSvgaAnimatedToPercentageHandler = { percentage in
+                    let manager = MarsSVGAPlayManager.shared
+                    if let currOp = manager.currOp {
+                        self.firstPercentages["\(currOp.hash)"] = percentage
+                    }
+                }
+                svgaPlayManager.onFinishAnimationHandler = {
+                    let manager = MarsSVGAPlayManager.shared
+                    if let currOp = manager.currOp {
+                        self.executeOperations()
+                        self.firstPercentages.removeValue(forKey: "\(currOp.hash)")
+                        if currOp === self.lastOp {
+                            self.lastOp = nil
+                        }
+                    }
+                }
+                
+                self.lastOp.queuePriority = .veryHigh
+                //if ops.count > 2 {
+                //    let secondOp = ops[1]
+                //    // 添加依赖存在问题，在执行的任务没有任何效果
+                //    secondOp.addDependency(lastOp)
+                //}
+                svgaPlayManager.finishAnimating()
+            }
+            
+            // 取消所有任务
+            //svgaPlayManager.opQueue.cancelAllOperations()
         }
+    }
+    
+    private func executeOperations() {
+        if self.firstOps.isEmpty { return }
+        let manager = MarsSVGAPlayManager.shared
+        let firstOp = self.firstOps.removeFirst()
+        
+        if let url = firstOp.svgaUrl {
+            manager.play(url: url)
+        } else if let name = firstOp.svgaName {
+            manager.play(named: name, inBundle: firstOp.inBundle)
+        }
+        
+        manager.operations.last?.queuePriority = .high
+        let percentage = self.firstPercentages["\(firstOp.hash)"] ?? 0.0
+        manager.step(toPercentage: percentage, andPlay: true)
     }
     
 }
